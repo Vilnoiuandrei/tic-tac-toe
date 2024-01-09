63b1ef354c30e39ccc41ec92d30f91f098f8df6f
Andrei Vilnoiu <Vilnoiuandrei@gmail.com>
2024-01-09 22:32:04 +0200

    commit 2

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ let squares = Array.from(document.getElementsByClassName("el"));
 let restartBtn = document.getElementById("restartBtn");
 let bord = document.querySelector(".main-component");
 
+let gameMode = "Multiplayer";
+let dificulty = document.querySelector("select");
+dificulty.addEventListener("change", function () {
+  return (gameMode = dificulty.options[dificulty.selectedIndex].text);
+});
+//selecting game mode
+
 const O_TEXT = "O";
 const X_TEXT = "X";
 let currentPlayer = X_TEXT;
+let ai = O_TEXT;
 let spaces = Array(9).fill(null);
 
 const startGame = () => {
@@ -23,12 +31,80 @@ function boxClicked(e) {
     if (playerHasWon() !== false) {
       bord.classList.add("stop-pres");
       playerText.innerHTML = `${currentPlayer} has won!`;
+    } else if (playerHasWon() === false && !spaces.includes(null)) {
+      playerText.innerHTML = "Tie";
+    } else {
+      if (gameMode === "Multiplayer") {
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+      }
+      if (gameMode === "Eazy") {
+        aiEazy();
+      }
+      if (gameMode === "Medium") {
+        aiMedium();
+      }
     }
-    if (playerHasWon() === false && !spaces.includes(null)) {
+  }
+}
+function aiEazy() {
+  const emptySpaces = spaces.reduce((acc, value, index) => {
+    if (!value) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
+  if (emptySpaces.length > 0) {
+    const randomIndex = Math.floor(Math.random() * emptySpaces.length);
+    const aiMove = emptySpaces[randomIndex];
+
+    spaces[aiMove] = ai;
+    function displayTextAi() {
+      squares[aiMove].innerText = ai;
+    }
+    setTimeout(displayTextAi, 600);
+
+    if (playerHasWon() !== false) {
+      bord.classList.add("stop-pres");
+      playerText.innerHTML = `${ai} has won!`;
+    } else if (playerHasWon() === false && !spaces.includes(null)) {
       playerText.innerHTML = "Tie";
     }
+  }
+}
+function aiMedium() {
+  function aiMove(n) {
+    spaces[n] = ai;
+    function displayTextAi() {
+      squares[n].innerText = ai;
+    }
+    setTimeout(displayTextAi, 600);
+  }
+  if (spaces[4] === null) {
+    aiMove(4);
+  } else if (spaces[0] === null) {
+    aiMove(0);
+  } else if (spaces[2] === null) {
+    aiMove(2);
+  } else if (spaces[6] === null) {
+    aiMove(6);
+  } else if (spaces[8] === null) {
+    aiMove(8);
+  } else if (spaces[1] === null) {
+    aiMove(1);
+  } else if (spaces[3] === null) {
+    aiMove(3);
+  } else if (spaces[5] === null) {
+    aiMove(5);
+  } else if (spaces[7] === null) {
+    aiMove(7);
+  }
 
-    currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+  if (playerHasWon() !== false) {
+    bord.classList.add("stop-pres");
+    playerText.innerHTML = `${ai} has won!`;
+  } else if (playerHasWon() === false && !spaces.includes(null)) {
+    playerText.innerHTML = "Tie";
   }
 }
 const winningCombos = [
